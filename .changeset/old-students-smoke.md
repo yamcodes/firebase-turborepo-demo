3821ee06a1a37fbacd20f5a2e91ed4b9768a1b35
Yam Borodetsky <hi@yam.codes>
2024-03-19 02:56:44 +0500

    chore: create www2

---
+++ .changeset/old-students-smoke.md
@@ -0,0 +1,5 @@
+---
+'www': minor
+---
+
+Make title clear
--- apps/www2/src/App.tsx
+++ apps/www2/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,10 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>www</h1>
+      <h1>www2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          The count is {count}
+          count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -28,11 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>
-        I am some cool text. Check me out!
-      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
