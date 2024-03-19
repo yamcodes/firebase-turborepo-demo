19f83082ea8bc9a9cfa564a6df3a32eaae1c3245
Yam Borodetsky <hi@yam.codes>
2024-03-19 20:45:32 +0600

    feat: add paragraphs (#31)

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -39,8 +39,9 @@ function App() {
           </li>
         </ul>
       </p>
+      <p>This is the best paragraph ever</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- apps/www2/src/App.tsx
+++ apps/www2/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,14 +28,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>
-        I'm cool text!
-      </p>
-      <p>
-        I'm another cool text. Check me out!
-      </p>
+      <p>I'm cool text!</p>
+      <p>I'm another cool text. Check me out!</p>
+      <p>Add paragraph in www2</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
