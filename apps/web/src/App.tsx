735826661281f7e14ed5c57ef7154d0fba1aeecb
Yam Borodetsky <hi@yam.codes>
2024-03-19 13:52:44 +0600

    fix(web): title in App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>yamcodes/my-turborepo</h1>
+      <h1>yamcodes/yamcodes</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
